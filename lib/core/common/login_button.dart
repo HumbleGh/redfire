308bb1c80bc8960a81d3040a7b1cd62c6c5ffb75
HumbleGh <isaacoseianane@gmail.com>
2023-11-15 16:02:22 +0000

    continue with google sign in completed

--- lib/core/common/login_button.dart
+++ lib/core/common/login_button.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:redfire/core/constants/constants.dart';
+import 'package:redfire/features/auth/controller/auth_controller.dart';
 import 'package:redfire/theme/app_layout.dart';
 import 'package:redfire/theme/pallete.dart';
 
-class LoginButton extends StatelessWidget {
+class LoginButton extends ConsumerWidget {
   const LoginButton({super.key});
 
+  // The Sign in function - Being created outside of the login button
+  // to avoid rewriting it all over again when the design of the button changes.
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).sigInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton.icon(
-      onPressed: () {},
+      onPressed: () => signInWithGoogle(ref),
       label: Text(
         "Continue with Google",
         style: GoogleFonts.manrope(fontSize: 18, fontWeight: FontWeight.w600),
