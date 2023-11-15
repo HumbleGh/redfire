387604cef3a5347e35c1d9e769036989d4a05c41
HumbleGh <isaacoseianane@gmail.com>
2023-11-15 13:20:34 +0000

    making progress

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -14,4 +16,26 @@ class AuthRepository {
   })  : _auth = auth,
         _fireStore = firestore,
         _googleSignIn = googleSignIn;
+
+  void signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      final googleAuth = await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // creating and storing user credentials to firestore(firebase)
+
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+
+      print(userCredential.user?.email);
+    } catch (e) {
+      print(e);
+    } // Avoid empty catch block
+  }
 }
