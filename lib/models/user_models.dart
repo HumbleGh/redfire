8f00f4b46e7ecab099f50da90eb5efd5019a053a
HumbleGh <isaacoseianane@gmail.com>
2023-11-15 18:09:36 +0000

    making progress

---
+++ lib/models/user_models.dart
@@ -0,0 +1,9 @@
+class UserModels {
+  final String name;
+  final String profilePic;
+  final String banner;
+  final String uid;
+  final bool isAuthenticated; //checks of user is guest or authenticated.
+  final int karma;
+  final List<String> awards;
+}
