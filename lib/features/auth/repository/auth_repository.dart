a00ef0121325c3e183965fc2c4debfb725ed08eb
HumbleGh <isaacoseianane@gmail.com>
2023-11-16 23:36:30 +0000

    making progress

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:redfire/core/constants/constants.dart';
+import 'package:redfire/core/constants/firebase_constants.dart';
+import 'package:redfire/models/user_models.dart';
 
 import '../../../core/providers/firebase_providers.dart';
 
@@ -26,6 +29,10 @@ class AuthRepository {
         _fireStore = firestore,
         _googleSignIn = googleSignIn;
 
+  // Firestore collection getter method
+  CollectionReference get _users =>
+      _fireStore.collection(FirebaseConstants.usersCollection);
+
   void signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -41,6 +48,20 @@ class AuthRepository {
 
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
+
+      // Getting user details from google
+
+      UserModels userModel = UserModels(
+        name: userCredential.user!.displayName ?? 'No Name',
+        profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
+        banner: Constants.bannerDefault,
+        uid: userCredential.user!.uid,
+        isAuthenticated: true,
+        karma: 0,
+        awards: [],
+      );
+
+      _users.doc();
 
       print(userCredential.user?.email);
     } catch (e) {
--- lib/models/user_models.dart
+++ lib/models/user_models.dart
@@ -1,3 +1,6 @@
+// import 'dart:convert';
+import 'package:flutter/foundation.dart';
+
 class UserModels {
   final String name;
   final String profilePic;
@@ -6,4 +9,95 @@ class UserModels {
   final bool isAuthenticated; //checks of user is guest or authenticated.
   final int karma;
   final List<String> awards;
+  UserModels({
+    required this.name,
+    required this.profilePic,
+    required this.banner,
+    required this.uid,
+    required this.isAuthenticated,
+    required this.karma,
+    required this.awards,
+  });
+
+  UserModels copyWith({
+    String? name,
+    String? profilePic,
+    String? banner,
+    String? uid,
+    bool? isAuthenticated,
+    int? karma,
+    List<String>? awards,
+  }) {
+    return UserModels(
+      name: name ?? this.name,
+      profilePic: profilePic ?? this.profilePic,
+      banner: banner ?? this.banner,
+      uid: uid ?? this.uid,
+      isAuthenticated: isAuthenticated ?? this.isAuthenticated,
+      karma: karma ?? this.karma,
+      awards: awards ?? this.awards,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'name': name});
+    result.addAll({'profilePic': profilePic});
+    result.addAll({'banner': banner});
+    result.addAll({'uid': uid});
+    result.addAll({'isAuthenticated': isAuthenticated});
+    result.addAll({'karma': karma});
+    result.addAll({'awards': awards});
+
+    return result;
+  }
+
+  factory UserModels.fromMap(Map<String, dynamic> map) {
+    return UserModels(
+      name: map['name'] ?? '',
+      profilePic: map['profilePic'] ?? '',
+      banner: map['banner'] ?? '',
+      uid: map['uid'] ?? '',
+      isAuthenticated: map['isAuthenticated'] ?? false,
+      karma: map['karma']?.toInt() ?? 0,
+      awards: List<String>.from(map['awards']),
+    );
+  }
+
+// This is required when getting tghe data from an api but since we atre utilizing firebase, it is not needed.
+  // String toJson() => json.encode(toMap());
+
+  // factory UserModels.fromJson(String source) =>
+  //     UserModels.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'UserModels(name: $name, profilePic: $profilePic, banner: $banner, uid: $uid, isAuthenticated: $isAuthenticated, karma: $karma, awards: $awards)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is UserModels &&
+        other.name == name &&
+        other.profilePic == profilePic &&
+        other.banner == banner &&
+        other.uid == uid &&
+        other.isAuthenticated == isAuthenticated &&
+        other.karma == karma &&
+        listEquals(other.awards, awards);
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        profilePic.hashCode ^
+        banner.hashCode ^
+        uid.hashCode ^
+        isAuthenticated.hashCode ^
+        karma.hashCode ^
+        awards.hashCode;
+  }
 }
