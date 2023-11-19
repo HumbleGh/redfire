b3886edb746c2dcbb43eda3a1f56f20a64e63c0c
HumbleGh <isaacoseianane@gmail.com>
2023-11-19 16:53:23 +0000

    making progress

---
+++ lib/core/common/loader.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class Loader extends StatelessWidget {
+  const Loader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: LoadingAnimationWidget.threeArchedCircle(
+          // leftDotColor: const Color(0xFF1A1A3F),
+          // rightDotColor: const Color(0xFFEA3799),
+          size: 50, color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:redfire/core/common/loader.dart';
 import 'package:redfire/core/common/login_button.dart';
 import 'package:redfire/core/constants/constants.dart';
+import 'package:redfire/features/auth/controller/auth_controller.dart';
 import 'package:redfire/theme/app_layout.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -19,35 +23,38 @@ class LoginScreen extends StatelessWidget {
         centerTitle: true,
         actions: [TextButton(onPressed: () {}, child: const Text("Skip"))],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-            horizontal: AppLayout.getHeight(20),
-            vertical: AppLayout.getHeight(20)),
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Text
-            Text(
-              "Dive into anything",
-              style: GoogleFonts.manrope(
-                  fontSize: 20, fontWeight: FontWeight.w600),
+      body: isLoading
+          ? const Loader()
+          : Container(
+              padding: EdgeInsets.symmetric(
+                  horizontal: AppLayout.getHeight(20),
+                  vertical: AppLayout.getHeight(20)),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //Text
+                  Text(
+                    "Dive into anything",
+                    style: GoogleFonts.manrope(
+                        fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                  Gap(AppLayout.getHeight(20)),
+                  //Image
+                  Image.asset(
+                    Constants.loginEmotePath,
+                    height: AppLayout.getHeight(400),
+                  ),
+                  const Spacer(),
+                  //Button
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(20)),
+                    child: const LoginButton(),
+                  )
+                ],
+              ),
             ),
-            Gap(AppLayout.getHeight(20)),
-            //Image
-            Image.asset(
-              Constants.loginEmotePath,
-              height: AppLayout.getHeight(400),
-            ),
-            const Spacer(),
-            //Button
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(20)),
-              child: const LoginButton(),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
